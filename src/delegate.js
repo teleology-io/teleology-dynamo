f4b8a54824481a992719bcc87bd6f38ed15edbaf
Chris Sullivan <chrissullivan.dev@gmail.com>
2020-08-14 00:36:53 -0700

    Adding delegate helper for wrapping aws calls

---
+++ src/delegate.js
@@ -0,0 +1,83 @@
+export const put = async ({ table: TableName, ddb, item }) => {
+  const { Attributes = {} } = await ddb
+    .put({
+      TableName,
+      Item: item,
+      ReturnValues: 'ALL_OLD',
+    })
+    .promise();
+
+  return {
+    ...Attributes,
+    ...item,
+  };
+};
+
+export const destroy = async ({ table: TableName, ddb, key, value }) =>
+  ddb
+    .delete({
+      TableName,
+      Key: {
+        [key]: value,
+      },
+    })
+    .promise();
+
+export const get = async ({ table: TableName, ddb, key, value }) => {
+  const response = await ddb
+    .get({
+      TableName,
+      Key: {
+        [key]: value,
+      },
+    })
+    .promise();
+
+  if (response && response.Item) return response.Item;
+};
+
+export const query = async ({ table: TableName, ddb, key, value, indexes }) => {
+  const foundIndex = indexes.find((it) => it.key === key);
+  if (!foundIndex) {
+    throw new Error('No index found for key', key);
+  }
+
+  const response = await ddb
+    .query({
+      TableName,
+      IndexName: foundIndex.name,
+      KeyConditionExpression: `${key} = :${key}`,
+      ExpressionAttributeValues: {
+        [`:${key}`]: value,
+      },
+    })
+    .promise();
+
+  return response && response.Items ? response.Items : [];
+};
+
+export const create = async ({ ddb, table, key, value, item }) => {
+  const exists = await get({ ddb, table, key, value, item });
+  if (exists) {
+    throw new Error(`A record already exists with that ${key}`);
+  }
+
+  return put({ ddb, table, item });
+};
+
+export const update = async ({ ddb, table, key, value, item }) => {
+  if (!key || !value) {
+    throw new Error('A primary key is needed to update a record');
+  }
+
+  const exists = await get({ ddb, table, key, value, item });
+
+  return put({
+    ddb,
+    table,
+    item: {
+      ...item,
+      ...exists,
+    },
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,88 +1,5 @@
 const AWS = require('aws-sdk');
-
-const put = async ({ ddb, table, item }) => {
-  const { Attributes = {} } = await ddb
-    .put({
-      TableName: table,
-      Item: item,
-      ReturnValues: 'ALL_OLD',
-    })
-    .promise();
-
-  return {
-    ...Attributes,
-    ...item,
-  };
-};
-
-const getter = async ({ ddb, table, key, value }) => {
-  const response = await ddb
-    .get({
-      TableName: table,
-      Key: {
-        [key]: value,
-      },
-    })
-    .promise();
-
-  if (response && response.Item) return response.Item;
-};
-
-const updater = async ({ ddb, table, key, value, item }) => {
-  if (!key || !value) {
-    throw new Error('A primary key is needed to update a record');
-  }
-
-  const exists = await getter({ ddb, table, key, value, item });
-  return put({
-    ddb,
-    table,
-    item: {
-      ...item,
-      ...exists,
-    },
-  });
-};
-
-const creator = async ({ ddb, table, key, value, item }) => {
-  const exists = await getter({ ddb, table, key, value, item });
-  if (exists) {
-    throw new Error(`A record already exists with that ${key}`);
-  }
-
-  return put({ ddb, table, item });
-};
-
-const destroy = async ({ ddb, table, key, value }) =>
-  ddb
-    .delete({
-      TableName: table,
-      Key: {
-        [key]: value,
-      },
-    })
-    .promise();
-
-const queryable = async ({ ddb, table, key, value, indexes }) => {
-  const foundIndex = indexes.find((it) => it.key === key);
-  if (!foundIndex) {
-    throw new Error('No index found for key', key);
-  }
-
-  const response = await ddb
-    .query({
-      TableName: table,
-      IndexName: foundIndex.name,
-      Limit: 5,
-      KeyConditionExpression: `${key} = :${key}`,
-      ExpressionAttributeValues: {
-        [`:${key}`]: value,
-      },
-    })
-    .promise();
-
-  return response && response.Items ? response.Items : [];
-};
+const delegate = require('./delegate');
 
 export default ({ table, key, indexes, awsOptions }) => {
   const ddb = new AWS.DynamoDB.DocumentClient({
@@ -99,7 +16,7 @@ export default ({ table, key, indexes, awsOptions }) => {
 
   return {
     get: (pk) =>
-      getter({
+      delegate.get({
         ...baseParams,
         value: pk,
       }),
@@ -107,23 +24,23 @@ export default ({ table, key, indexes, awsOptions }) => {
     create: (item) => {
       const { [key]: value } = item;
 
-      return creator({ ...baseParams, value, item });
+      return delegate.create({ ...baseParams, value, item });
     },
 
     update: (item) => {
       const { [key]: value } = item;
-      return updater({ ...baseParams, value, item });
+      return delegate.update({ ...baseParams, value, item });
     },
 
     delete: (pk) =>
-      destroy({
+      delegate.destroy({
         ...baseParams,
         value: pk,
       }),
 
     query: (keyVal) => {
       const [firstKey] = Object.keys(keyVal);
-      return queryable({
+      return delegate.query({
         ...baseParams,
         key: firstKey,
         value: keyVal[firstKey],
